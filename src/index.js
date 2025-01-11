7ef3f50a8d3157f16ceef34d02cb5f24451f6486
Abdulrahman-Abdulhak <bdalrhmanbdalhq@gmail.com>
2025-01-11 18:26:06 +0300

    Making use of environment variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -8,8 +10,8 @@ const port = 3000;
 // Middleware
 app.use(bodyParser.json());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
--- src/models/User.js
+++ src/models/User.js
@@ -1,41 +1,42 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize('test', 'munier', 'moner1234', {
-    host: 'localhost',
-    dialect: 'mysql'
+const env = { process };
+const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
+  host: env.DB_HOST,
+  dialect: "mysql",
 });
 
-const User = sequelize.define('User', {
-    fullName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    userType: {
-        type: DataTypes.ENUM('Employee', 'Visitor'),
-        allowNull: false
-    },
-    phoneNumber: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    carPlate: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    }
+const User = sequelize.define("User", {
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userType: {
+    type: DataTypes.ENUM("Employee", "Visitor"),
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  carPlate: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
 });
 
 const createUsersTable = async () => {
-    await User.sync();
+  await User.sync();
 };
 
 module.exports = { User, createUsersTable };
